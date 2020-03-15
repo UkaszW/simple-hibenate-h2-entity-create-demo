229444281e25ddc4edd5280b50fd77a86da6fb10
Łukasz Wójcik <lukasz.wojcik@comarch.pl>
2020-03-15 15:39:07 +0100

    initialize project

---
+++ src/main/java/com/example/hibenateh2entitycreate/HibenateH2EntityCreateApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibenateh2entitycreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibenateH2EntityCreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibenateH2EntityCreateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hibenateh2entitycreate/HibenateH2EntityCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibenateh2entitycreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibenateH2EntityCreateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
